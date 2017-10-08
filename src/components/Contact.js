7a3a8824cfbdeb1820c7a5e48dfe701775a9ef97
blazeka <adam.blazek@merck.com>
2017-10-08 19:54:42 +0200

    improve Welcome and Contact page styling of background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,45 +27,51 @@ class Contact extends React.PureComponent {
 	render() {
     return (
 			<div className="Contact">
-				<div className="Contact-bg"/>
-					<h1>Kontakt</h1>
-					<div className="Contact-content">
-						<form onSubmit={this.handleFormSubmit}>
-							<div>
-								<label htmlFor="firstName">Jméno</label>
-								<input type="text" id="firstName" onChange={this.handleChange('firstName')} />
-								<label htmlFor="lastName">Příjmení</label>
-								<input type="text" id="lastName" onChange={this.handleChange('lastName')} />
-							</div>
-							<div>
-								<label htmlFor="email">E-mail</label>
-								<input type="email" id="email" onChange={this.handleChange('email')} />
-							</div>
-							<div>
-								<label htmlFor="phone">Telefon</label>
-								<input type="text" id="phone" onChange={this.handleChange('phone')} />
-							</div>
-							<div>
-								<label htmlFor="msg">Zpráva</label>
-								<textarea id="msg" onChange={this.handleChange('msg')} />
-							</div>
-							<button type="submit">Odeslat</button>
-						</form>
-						<div className="Contact-info">
-							<div>
-								Jmeno Prijmeni
-							</div>
-							<div>
-								<b>Telefon</b><br/>
-								<b>E-Mail</b>
-							</div>
-							<div>
-								Ulice<br/>
-								Mesto<br/>
-								PSC
-							</div>
-					  </div>
-				  </div>
+				<div className="Contact-bg">
+					<span />
+					<span />
+					<span />
+					<span />
+					<span />
+				</div>
+				<h1>Kontakt</h1>
+				<div className="Contact-content">
+					<form onSubmit={this.handleFormSubmit}>
+						<div>
+							<label htmlFor="firstName">Jméno</label>
+							<input type="text" id="firstName" onChange={this.handleChange('firstName')} />
+							<label htmlFor="lastName">Příjmení</label>
+							<input type="text" id="lastName" onChange={this.handleChange('lastName')} />
+						</div>
+						<div>
+							<label htmlFor="email">E-mail</label>
+							<input type="email" id="email" onChange={this.handleChange('email')} />
+						</div>
+						<div>
+							<label htmlFor="phone">Telefon</label>
+							<input type="text" id="phone" onChange={this.handleChange('phone')} />
+						</div>
+						<div>
+							<label htmlFor="msg">Zpráva</label>
+							<textarea id="msg" onChange={this.handleChange('msg')} />
+						</div>
+						<button type="submit">Odeslat</button>
+					</form>
+					<div className="Contact-info">
+						<div>
+							Jmeno Prijmeni
+						</div>
+						<div>
+							<b>Telefon</b><br/>
+							<b>E-Mail</b>
+						</div>
+						<div>
+							Ulice<br/>
+							Mesto<br/>
+							PSC
+						</div>
+					</div>
+				</div>
 		  </div>)
 	}
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Welcome = () =>
 <div className="Welcome">
-	<div className="Welcome-bg" />
+	<div className="Welcome-bg">
+		<span />
+		<span />
+		<span />
+		<span />
+		<span />
+	</div>
+
 	<div className="Welcome-content">
 		<h1>Arbovert</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident..</p>
