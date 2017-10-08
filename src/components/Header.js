2496c2f1ec2e71199fd23c97db7382783f235315
blazeka <adam.blazek@merck.com>
2017-10-08 21:42:15 +0200

    kick-off of Service page, onClick scrolling to Contact/Welcome page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-import '../styles/Header.css'
 import Scroll from 'react-scroll'
 import logo from '../assets/logo.svg'
+import '../styles/Header.css'
 
-var Link = Scroll.Link;
+const Link = Scroll.Link
+const scroller = Scroll.scroller
+
+const scrollToWelcome = () => () => {
+	scroller.scrollTo('welcome', {duration: 1500, delay: 100, smooth: true})
+}
 
 const Header = (props) =>
 	<nav className="Header-nav">
 		<div className="Header-logoName">
-			<img src={logo}/>
-			<div className="Header-name">
-				ARBOVERT
-			</div>
+			<a onClick={scrollToWelcome()}>
+				<img src={logo}/>
+				<span>ARBOVERT</span>
+			</a>
 		</div>
 		<div className="Header-spacer" />
 		<ul>
--- src/components/Service.js
+++ src/components/Service.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import Scroll from 'react-scroll'
 import '../styles/Service.css'
+
+const scroller = Scroll.scroller
+
+const scrollToContact = () => () => {
+	scroller.scrollTo('contact', {duration: 1500, delay: 100, smooth: true})
+}
 
 const Service = () =>
 	<div className="Service">
 		<h1>Služby</h1>
+		<div className="Service-content">
+			<div>
+				<div className="Service-box">
+					<h2>Sluzba 1</h2>
+					<div>
+						Neco o prvni sluzbe
+					</div>
+				</div>
+				<div className="Service-box">
+					<h2>Sluzba 2</h2>
+					<div>
+						Neco o druhe
+					</div>
+				</div>
+			</div>
+			<div>
+				<div className="Service-box">
+					<h2>Sluzba 3</h2>
+					<div>
+						O treti
+					</div>
+				</div>
+				<div className="Service-box">
+					<h2>Sluzba 4</h2>
+					<div>
+						A ctvrte
+					</div>
+				</div>
+			</div>
+		</div>
+		<div className="Service-contact">
+			<h2>
+				Rad bych si Vas objednal.
+			</h2>
+			<button value="Test 2" onClick={scrollToContact()} >
+				Test 2
+			</button>
+		</div>
 	</div>
 
 export default Service
