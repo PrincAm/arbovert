fc0f062853fb2f48eb31f9e17d89e12defdd6f6a
blazeka <Jitrocel13>
2018-01-28 21:45:41 +0100

    header styling and transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,36 +7,38 @@ const Link = Scroll.Link
 const scroller = Scroll.scroller
 
 const scrollToWelcome = () => {
-	scroller.scrollTo('welcome', {duration: 1500, delay: 100, smooth: true, offset: -70})
+	scroller.scrollTo('welcome', {duration: 2000, delay: 100, smooth: "easeInOutQuad", offset: -70})
 }
 
 const Header = (props) =>
 	<nav className="Header-nav">
-		<div className="Header-logoName">
+		<div className="Header-logo">
 			<a onClick={scrollToWelcome}>
-				<img alt="arbovert-logo" src={logo}/>
-				<span>ARBOVERT</span>
+        {/*<img alt="arbovert-logo" src={logo}/>*/}
+				<span>Arbovert</span>
 			</a>
 		</div>
 		<div className="Header-spacer" />
-		<ul>
-			<li>
-				<Link activeClass="active" className="Header-welcome" to="welcome"
-							spy={true} smooth={true} duration={500} offset={-70}>Domů</Link>
-			</li>
-			<li>
-				<Link activeClass="active" className="Header-service" to="service"
-							spy={true} smooth={true} duration={500} >Služby</Link>
-			</li>
-			<li>
-				<Link activeClass="active" className="Header-reference" to="reference"
-							spy={true} smooth={true} duration={500}>Reference</Link>
-			</li>
-			<li>
-				<Link activeClass="active" className="Header-contact" to="contact"
-							spy={true} smooth={true} duration={500}>Kontakt</Link>
-			</li>
-		</ul>
+    <div className="Header-menu">
+      <ul>
+        <li>
+          <Link activeClass="active" className="Header-welcome" to="welcome"
+                spy={true} smooth={"easeInOutQuad"} duration={2000} delay={100} offset={-70}>Domů</Link>
+        </li>
+        <li>
+          <Link activeClass="active" className="Header-service" to="service"
+                spy={true} smooth={"easeInOutQuad"} duration={2000} delay={100}>Služby</Link>
+        </li>
+        <li>
+          <Link activeClass="active" className="Header-reference" to="reference"
+                spy={true} smooth={"easeInOutQuad"} duration={2000} delay={100}>Reference</Link>
+        </li>
+        <li>
+          <Link activeClass="active" className="Header-contact" to="contact"
+                spy={true} smooth={"easeInOutQuad"} duration={2000} delay={100}>Kontakt</Link>
+        </li>
+      </ul>
+    </div>
 	</nav>
 
 export default Header
