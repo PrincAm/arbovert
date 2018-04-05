9786e6c4112b422d9290c6c017433166d48dcc9d
PrincAm <ratbo90210@gmail.com>
2018-04-05 23:31:26 +0200

    contact styling and adding of the map image

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,38 @@
 import React from 'react'
-// import map from '../assets/map.png'
+import map from '../assets/map.png'
 import '../styles/Contact.css'
 
 const Contact = () => (
   <div className="Contact">
     <div className="Contact-content">
-      <div className="Contact-inner-div">
-        <h1>Kontaktujte nás</h1>
-        <div className="Contact-companyName">
-          Arbovert
+      <h1>Kontaktujte nás</h1>
+      <div className="Contact-upperDiv">
+        <div className="Contact-inner-div">
+          <div className="Contact-companyName">
+            Arbovert
+          </div>
+          <div>
+            Lukáš Kačer
+          </div>
+          <div>
+            <a href="tel:+420739969933">(+420) 739 969 933</a><br/>
+            <a href="mailto:lukas@arbovert.cz">lukas@arbovert.cz</a>
+          </div>
         </div>
-        <div>
-          Lukáš Kačer
-        </div>
-        <div>
-          <a href="tel:+420739969933">(+420) 739 969 933</a><br/>
-          <a href="mailto:lukas@arbovert.cz">lukas@arbovert.cz</a>
-        </div>
-      </div>
-      {/*
-      <div className="Contact-inner-div-map">
-        <img style={{paddingLeft: '20px'}} src={map} />
-        <div style={{fontSize: '0.8em'}}>
-          Pusobime v Praze a stredoceskem kraji.
+        <div className="Contact-map">
+          <img src={map} />
         </div>
       </div>
-      */}
-      <div style={{padding: '50px', textAlign: 'center'}}>
-        <a href="https://facebook.com" class="icon-button facebook">
-          <i class="icon-facebook" />
+      <div>
+        Pusobime v Praze a stredoceskem kraji
+      </div>
+      <div className="Contact-socialMedia">
+        <a href="https://facebook.com" className="icon-button facebook">
+          <i className="icon-facebook" />
           <span />
         </a>
-        <a href="https://plus.google.com" class="icon-button google-plus">
-          <i class="icon-google-plus" />
+        <a href="https://plus.google.com" className="icon-button google-plus">
+          <i className="icon-google-plus" />
           <span />
         </a>
       </div>
