658bcb17451077bf890ab480590266bf3d7b4dfc
PrincAm <ratbo90210@gmail.com>
2019-01-09 11:12:05 +0100

    wrong grammar in company description

--- src/components/Company.js
+++ src/components/Company.js
@@ -29,7 +29,7 @@ const Service = () => {
             />
           </picture>
           Náš Arbovert se již od roku 2013 zabývá arboristikou, kompletní péčí o dřeviny a výškovými pracemi všeho
-          druhu. Pokud Vás tíží nebezpečně přerostlé stromy, opadávající omýtka, dotěrní holubi nebo strom trefený
+          druhu. Pokud Vás tíží nebezpečně přerostlé stromy, opadávající omítka, dotěrní holubi nebo strom trefený
           bleskem, zavolejte a my s vámi váš problém rádi probereme. Klidně se na nás obraťte, ikdyž se jedná o
           havarijní situace v nepopulární čas. Snad ani nemusíme zmiňovat, že nezávazné nacenění práce je samozřejmostí.
           Zakládáme si na odbornosti našich lidí a našim cílem je se zdokonalovat a zlepšovat v pracích, u kterých
