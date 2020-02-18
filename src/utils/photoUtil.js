b56d6bd373f598b4fb958acaa46fb3ae038e01d4
Adam <ablazek@securityscorecard.io>
2020-02-18 22:00:38 +0100

    update photos in gallery

--- src/utils/photoUtil.js
+++ src/utils/photoUtil.js
@@ -1,10 +1,3 @@
-import arbo4_200 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_200.jpg'
-import arbo4_263 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_263.jpg'
-import arbo4_317 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_317.jpg'
-import arbo4_366 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_366.jpg'
-import arbo4_411 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_411.jpg'
-import arbo4_576 from '../assets/photos/arbo/arbo4_oyptwm_c_scale,w_576.jpg'
-
 import arbo5_200 from '../assets/photos/arbo/arbo5_kohiwu_c_scale,w_200.jpg'
 import arbo5_262 from '../assets/photos/arbo/arbo5_kohiwu_c_scale,w_262.jpg'
 import arbo5_315 from '../assets/photos/arbo/arbo5_kohiwu_c_scale,w_315.jpg'
@@ -19,25 +12,11 @@ import arbo6_344 from '../assets/photos/arbo/arbo6_cmf4sw_c_scale,w_344.jpg'
 import arbo6_405 from '../assets/photos/arbo/arbo6_cmf4sw_c_scale,w_405.jpg'
 import arbo6_576 from '../assets/photos/arbo/arbo6_cmf4sw_c_scale,w_576.jpg'
 
-import arbo7_200 from '../assets/photos/arbo/arbo7_ttgiit_c_scale,w_200.jpg'
-import arbo7_274 from '../assets/photos/arbo/arbo7_ttgiit_c_scale,w_274.jpg'
-import arbo7_338 from '../assets/photos/arbo/arbo7_ttgiit_c_scale,w_338.jpg'
-import arbo7_398 from '../assets/photos/arbo/arbo7_ttgiit_c_scale,w_398.jpg'
-import arbo7_576 from '../assets/photos/arbo/arbo7_ttgiit_c_scale,w_576.jpg'
-
 import arbo8_200 from '../assets/photos/arbo/arbo8_kpz6a8_c_scale,w_200.jpg'
 import arbo8_273 from '../assets/photos/arbo/arbo8_kpz6a8_c_scale,w_273.jpg'
 import arbo8_335 from '../assets/photos/arbo/arbo8_kpz6a8_c_scale,w_335.jpg'
 import arbo8_389 from '../assets/photos/arbo/arbo8_kpz6a8_c_scale,w_389.jpg'
 import arbo8_576 from '../assets/photos/arbo/arbo8_kpz6a8_c_scale,w_576.jpg'
-
-import arbo9_200 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_200.jpg'
-import arbo9_338 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_338.jpg'
-import arbo9_411 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_441.jpg'
-import arbo9_528 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_528.jpg'
-import arbo9_602 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_602.jpg'
-import arbo9_755 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_755.jpg'
-import arbo9_1024 from '../assets/photos/arbo/arbo9_lfcn2d_c_scale,w_1024.jpg'
 
 import arbo10_200 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_200.jpg'
 import arbo10_350 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_350.jpg'
@@ -46,14 +25,6 @@ import arbo10_557 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_557.jpg'
 import arbo10_634 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_634.jpg'
 import arbo10_701 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_701.jpg'
 import arbo10_1024 from '../assets/photos/arbo/arbo10_wydsxu_c_scale,w_1024.jpg'
-
-import upwork8_200 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_200.jpg'
-import upwork8_375 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_375.jpg'
-import upwork8_576 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_576.jpg'
-
-import upwork11_200 from '../assets/photos/upwork/upwork11_iml1dt_c_scale,w_200.jpg'
-import upwork11_576 from '../assets/photos/upwork/upwork11_iml1dt_c_scale,w_576.jpg'
-
 
 import arbo20_200 from '../assets/photos/arbo/arbo20_o_aeszch_c_scale,w_200.jpg'
 import arbo20_630 from '../assets/photos/arbo/arbo20_o_aeszch_c_scale,w_630.jpg'
@@ -85,12 +56,6 @@ import arbo24_945 from '../assets/photos/arbo/arbo24_o_lu3saj_c_scale,w_945.jpg'
 import arbo24_1224 from '../assets/photos/arbo/arbo24_o_lu3saj_c_scale,w_1224.jpg'
 import arbo24_1400 from '../assets/photos/arbo/arbo24_o_lu3saj_c_scale,w_1400.jpg'
 
-import arbo25_200 from '../assets/photos/arbo/arbo25_o_lwljup_c_scale,w_200.jpg'
-import arbo25_658 from '../assets/photos/arbo/arbo25_o_lwljup_c_scale,w_658.jpg'
-import arbo25_947 from '../assets/photos/arbo/arbo25_o_lwljup_c_scale,w_947.jpg'
-import arbo25_1225 from '../assets/photos/arbo/arbo25_o_lwljup_c_scale,w_1225.jpg'
-import arbo25_1400 from '../assets/photos/arbo/arbo25_o_lwljup_c_scale,w_1400.jpg'
-
 import arbo26_200 from '../assets/photos/arbo/arbo26_o_ihjsbh_c_scale,w_200.jpg'
 import arbo26_645 from '../assets/photos/arbo/arbo26_o_ihjsbh_c_scale,w_645.jpg'
 import arbo26_943 from '../assets/photos/arbo/arbo26_o_ihjsbh_c_scale,w_943.jpg'
@@ -115,12 +80,6 @@ import arbo29_958 from '../assets/photos/arbo/arbo29_o_wruc2x_c_scale,w_958.jpg'
 import arbo29_1237 from '../assets/photos/arbo/arbo29_o_wruc2x_c_scale,w_1237.jpg'
 import arbo29_1400 from '../assets/photos/arbo/arbo29_o_wruc2x_c_scale,w_1400.jpg'
 
-import arbo30_200 from '../assets/photos/arbo/arbo30_o_x2ee2d_c_scale,w_200.jpg'
-import arbo30_701 from '../assets/photos/arbo/arbo30_o_x2ee2d_c_scale,w_701.jpg'
-import arbo30_1020 from '../assets/photos/arbo/arbo30_o_x2ee2d_c_scale,w_1020.jpg'
-import arbo30_1285 from '../assets/photos/arbo/arbo30_o_x2ee2d_c_scale,w_1285.jpg'
-import arbo30_1400 from '../assets/photos/arbo/arbo30_o_x2ee2d_c_scale,w_1400.jpg'
-
 import arbo31_200 from '../assets/photos/arbo/arbo31_c_scale,w_200.jpg'
 import arbo31_670 from '../assets/photos/arbo/arbo31_c_scale,w_670.jpg'
 import arbo31_954 from '../assets/photos/arbo/arbo31_c_scale,w_954.jpg'
@@ -132,12 +91,6 @@ import arbo32_714 from '../assets/photos/arbo/arbo32_c_scale,w_714.jpg'
 import arbo32_1036 from '../assets/photos/arbo/arbo32_c_scale,w_1036.jpg'
 import arbo32_1299 from '../assets/photos/arbo/arbo32_c_scale,w_1299.jpg'
 import arbo32_1400 from '../assets/photos/arbo/arbo32_c_scale,w_1400.jpg'
-
-import arbo33_200 from '../assets/photos/arbo/arbo33_c_scale,w_200.jpg'
-import arbo33_704 from '../assets/photos/arbo/arbo33_c_scale,w_704.jpg'
-import arbo33_1020 from '../assets/photos/arbo/arbo33_c_scale,w_1020.jpg'
-import arbo33_1283 from '../assets/photos/arbo/arbo33_c_scale,w_1283.jpg'
-import arbo33_1400 from '../assets/photos/arbo/arbo33_c_scale,w_1400.jpg'
 
 import arbo34_200 from '../assets/photos/arbo/arbo34_c_scale,w_200.jpg'
 import arbo34_677 from '../assets/photos/arbo/arbo34_c_scale,w_677.jpg'
@@ -157,12 +110,6 @@ import arbo36_997 from '../assets/photos/arbo/arbo36_c_scale,w_997.jpg'
 import arbo36_1252 from '../assets/photos/arbo/arbo36_c_scale,w_1252.jpg'
 import arbo36_1400 from '../assets/photos/arbo/arbo36_c_scale,w_1400.jpg'
 
-import arbo37_200 from '../assets/photos/arbo/arbo37_c_scale,w_200.jpg'
-import arbo37_683 from '../assets/photos/arbo/arbo37_c_scale,w_683.jpg'
-import arbo37_962 from '../assets/photos/arbo/arbo37_c_scale,w_962.jpg'
-import arbo37_1245 from '../assets/photos/arbo/arbo37_c_scale,w_1245.jpg'
-import arbo37_1400 from '../assets/photos/arbo/arbo37_c_scale,w_1400.jpg'
-
 import arbo38_200 from '../assets/photos/arbo/arbo38_c_scale,w_200.jpg'
 import arbo38_696 from '../assets/photos/arbo/arbo38_c_scale,w_696.jpg'
 import arbo38_1024 from '../assets/photos/arbo/arbo38_c_scale,w_1024.jpg'
@@ -175,27 +122,63 @@ import arbo39_1005 from '../assets/photos/arbo/arbo39_c_scale,w_1005.jpg'
 import arbo39_1267 from '../assets/photos/arbo/arbo39_c_scale,w_1267.jpg'
 import arbo39_1400 from '../assets/photos/arbo/arbo39_c_scale,w_1400.jpg'
 
-import arbo40_200 from '../assets/photos/arbo/arbo40_c_scale,w_200.jpg'
-import arbo40_673 from '../assets/photos/arbo/arbo40_c_scale,w_673.jpg'
-import arbo40_956 from '../assets/photos/arbo/arbo40_c_scale,w_956.jpg'
-import arbo40_1237 from '../assets/photos/arbo/arbo40_c_scale,w_1237.jpg'
-import arbo40_1400 from '../assets/photos/arbo/arbo40_c_scale,w_1400.jpg'
+import upwork1_200 from '../assets/photos/upwork/upwork1_gfyyvb_c_scale,w_200.jpg'
+import upwork1_511 from '../assets/photos/upwork/upwork1_gfyyvb_c_scale,w_511.jpg'
+
+import upwork2_200 from '../assets/photos/upwork/upwork2_l18gph_c_scale,w_200.jpg'
+import upwork2_350 from '../assets/photos/upwork/upwork2_l18gph_c_scale,w_350.jpg'
+import upwork2_511 from '../assets/photos/upwork/upwork2_l18gph_c_scale,w_511.jpg'
+
+import upwork3_200 from '../assets/photos/upwork/upwork3_xevgea_c_scale,w_200.jpg'
+import upwork3_450 from '../assets/photos/upwork/upwork3_xevgea_c_scale,w_458.jpg'
+import upwork3_647 from '../assets/photos/upwork/upwork3_xevgea_c_scale,w_647.jpg'
+import upwork3_1024 from '../assets/photos/upwork/upwork3_xevgea_c_scale,w_1024.jpg'
+
+import upwork4_200 from '../assets/photos/upwork/upwork4_mqkp3m_c_scale,w_200.jpg'
+import upwork4_511 from '../assets/photos/upwork/upwork4_mqkp3m_c_scale,w_511.jpg'
+
+import upwork5_200 from '../assets/photos/upwork/upwork5_yfp8ul_c_scale,w_200.jpg'
+import upwork5_511 from '../assets/photos/upwork/upwork5_yfp8ul_c_scale,w_511.jpg'
+
+import upwork6_200 from '../assets/photos/upwork/upwork6_c5iiy9_c_scale,w_200.jpg'
+import upwork6_318 from '../assets/photos/upwork/upwork6_c5iiy9_c_scale,w_318.jpg'
+import upwork6_576 from '../assets/photos/upwork/upwork6_c5iiy9_c_scale,w_576.jpg'
+
+import upwork7_200 from '../assets/photos/upwork/upwork7_onyqa3_c_scale,w_200.jpg'
+import upwork7_576 from '../assets/photos/upwork/upwork7_onyqa3_c_scale,w_576.jpg'
+
+import upwork8_200 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_200.jpg'
+import upwork8_375 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_375.jpg'
+import upwork8_576 from '../assets/photos/upwork/upwork8_xouai6_c_scale,w_576.jpg'
+
+import upwork9_200 from '../assets/photos/upwork/upwork9_augkxi_c_scale,w_200.jpg'
+import upwork9_576 from '../assets/photos/upwork/upwork9_augkxi_c_scale,w_576.jpg'
+
+import upwork10_200 from '../assets/photos/upwork/upwork10_cgtqik_c_scale,w_200.jpg'
+import upwork10_375 from '../assets/photos/upwork/upwork10_cgtqik_c_scale,w_375.jpg'
+import upwork10_576 from '../assets/photos/upwork/upwork10_cgtqik_c_scale,w_576.jpg'
+
+import upwork11_200 from '../assets/photos/upwork/upwork11_iml1dt_c_scale,w_200.jpg'
+import upwork11_576 from '../assets/photos/upwork/upwork11_iml1dt_c_scale,w_576.jpg'
+
+import upwork13_200 from '../assets/photos/upwork/upwork13_iijzt6_c_scale,w_200.jpg'
+import upwork13_396 from '../assets/photos/upwork/upwork13_iijzt6_c_scale,w_396.jpg'
+import upwork13_542 from '../assets/photos/upwork/upwork13_iijzt6_c_scale,w_542.jpg'
+
+import upwork14_200 from '../assets/photos/upwork/upwork14_muupcd_c_scale,w_200.jpg'
+import upwork14_388 from '../assets/photos/upwork/upwork14_muupcd_c_scale,w_388.jpg'
+import upwork14_534 from '../assets/photos/upwork/upwork14_muupcd_c_scale,w_534.jpg'
+
+import upwork15_200 from '../assets/photos/upwork/upwork15_b364z9_c_scale,w_200.jpg'
+import upwork15_384 from '../assets/photos/upwork/upwork15_b364z9_c_scale,w_384.jpg'
+import upwork15_517 from '../assets/photos/upwork/upwork15_b364z9_c_scale,w_517.jpg'
+
+import upwork16_200 from '../assets/photos/upwork/upwork16_gyogkx_c_scale,w_200.jpg'
+import upwork16_372 from '../assets/photos/upwork/upwork16_gyogkx_c_scale,w_372.jpg'
+import upwork16_492 from '../assets/photos/upwork/upwork16_gyogkx_c_scale,w_492.jpg'
+import upwork16_591 from '../assets/photos/upwork/upwork16_gyogkx_c_scale,w_591.jpg'
 
 export const PHOTOS1 = [
-  {
-    sizes: ['(max-width: 1400px) 100vw', '1400px'],
-    srcSet: [
-      `${arbo40_200} 200w`,
-      `${arbo40_673} 673w`,
-      `${arbo40_956} 956w`,
-      `${arbo40_1237} 1237w`,
-      `${arbo40_1400} 1400w`
-    ],
-    src: arbo40_673,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
-  },
   {
     sizes: ['(max-width: 1400px) 100vw', '1400px'],
     srcSet: [
@@ -223,20 +206,6 @@ export const PHOTOS1 = [
     alt: 'arborist work',
     width: 4,
     height: 3
-  },
-  {
-    sizes: ['(max-width: 1400px) 100vw', '1400px'],
-    srcSet: [
-      `${arbo37_200} 200w`,
-      `${arbo37_683} 683w`,
-      `${arbo37_962} 962w`,
-      `${arbo37_1245} 1245w`,
-      `${arbo37_1400} 1400w`
-    ],
-    src: arbo37_683,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
   },
   {
     sizes: ['(max-width: 1400px) 100vw', '1400px'],
@@ -283,20 +252,6 @@ export const PHOTOS1 = [
   {
     sizes: ['(max-width: 1400px) 100vw', '1400px'],
     srcSet: [
-      `${arbo33_200} 200w`,
-      `${arbo33_704} 704w`,
-      `${arbo33_1020} 1020w`,
-      `${arbo33_1283} 1283w`,
-      `${arbo33_1400} 1400w`
-    ],
-    src: arbo33_704,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
-  },
-  {
-    sizes: ['(max-width: 1400px) 100vw', '1400px'],
-    srcSet: [
       `${arbo32_200} 200w`,
       `${arbo32_714} 714w`,
       `${arbo32_1036} 1036w`,
@@ -318,20 +273,6 @@ export const PHOTOS1 = [
       `${arbo31_1400} 1400w`
     ],
     src: arbo31_670,
-    alt: 'arborist work',
-    width: 4,
-    height: 3
-  },
-  {
-    sizes: ['(max-width: 1400px) 100vw', '1400px'],
-    srcSet: [
-      `${arbo30_200} 200w`,
-      `${arbo30_701} 701w`,
-      `${arbo30_1020} 1020w`,
-      `${arbo30_1285} 1285w`,
-      `${arbo30_1400} 1400w`
-    ],
-    src: arbo30_701,
     alt: 'arborist work',
     width: 4,
     height: 3
@@ -388,20 +329,6 @@ export const PHOTOS1 = [
       `${arbo26_1400} 1400w`
     ],
     src: arbo26_645,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
-  },
-  {
-    sizes: ['(max-width: 1400px) 100vw', '1400px'],
-    srcSet: [
-      `${arbo25_200} 200w`,
-      `${arbo25_658} 658w`,
-      `${arbo25_947} 947w`,
-      `${arbo25_1225} 1225w`,
-      `${arbo25_1400} 1400w`
-    ],
-    src: arbo25_658,
     alt: 'arborist work',
     width: 3,
     height: 4
@@ -479,21 +406,6 @@ export const PHOTOS1 = [
   {
     sizes: ['(max-width: 576px) 100vw', '576px'],
     srcSet: [
-      `${arbo4_200} 200w`,
-      `${arbo4_263} 263w`,
-      `${arbo4_317} 317w`,
-      `${arbo4_366} 366w`,
-      `${arbo4_411} 411w`,
-      `${arbo4_576} 576w`
-    ],
-    src: arbo4_576,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
-  },
-  {
-    sizes: ['(max-width: 576px) 100vw', '576px'],
-    srcSet: [
       `${arbo5_200} 200w`,
       `${arbo5_262} 262w`,
       `${arbo5_315} 315w`,
@@ -517,35 +429,11 @@ export const PHOTOS1 = [
   },
   {
     sizes: ['(max-width: 576px) 100vw', '576px'],
-    srcSet: [`${arbo7_200} 200w`, `${arbo7_274} 274w`, `${arbo7_338} 338w`, `${arbo7_398} 498w`, `${arbo7_576} 576w`],
-    src: arbo7_576,
-    alt: 'arborist work',
-    width: 3,
-    height: 4
-  },
-  {
-    sizes: ['(max-width: 576px) 100vw', '576px'],
     srcSet: [`${arbo8_200} 200w`, `${arbo8_273} 273w`, `${arbo8_335} 335w`, `${arbo8_389} 389w`, `${arbo8_576} 576w`],
     src: arbo8_576,
     alt: 'arborist work',
     width: 3,
     height: 4
-  },
-  {
-    sizes: ['(max-width: 1024px) 100vw', '1024px'],
-    srcSet: [
-      `${arbo9_200} 200w`,
-      `${arbo9_338} 338w`,
-      `${arbo9_411} 411w`,
-      `${arbo9_528} 528w`,
-      `${arbo9_602} 602w`,
-      `${arbo9_755} 755w`,
-      `${arbo9_1024} 1024w`
-    ],
-    src: arbo9_1024,
-    alt: 'arborist work',
-    width: 4,
-    height: 3
   },
   {
     sizes: ['(max-width: 1024px) 100vw', '1024px'],
@@ -565,17 +453,120 @@ export const PHOTOS1 = [
   },
   {
     sizes: ['(max-width: 576) 100vw', '576px'],
-    srcSet: [`${upwork8_200} 200w`, `${upwork8_375} 375w`, `${upwork8_576} 576w`],
+    srcSet: [`${upwork1_200} 200w`, `${upwork1_511} 511w`],
+    src: upwork1_511,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcSet: [`${upwork2_200} 200w`, `${upwork2_350} 350w`, `${upwork2_511} 511w`],
+    src: upwork2_511,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 1024) 100vw', '1024px'],
+    srcset: [`${upwork3_200} 200w`, `${upwork3_450} 450w`, `${upwork3_647} 647w`, `${upwork3_1024} 1024w`],
+    src: upwork3_1024,
+    alt: 'upwork',
+    width: 16,
+    height: 9
+  },
+  {
+    sizes: ['(max-width: 511) 100vw', '511px'],
+    srcset: [`${upwork4_200} 200w`, `${upwork4_511} 511w`],
+    src: upwork4_511,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 511) 100vw', '511px'],
+    srcset: [`${upwork5_200} 200w`, `${upwork5_511} 511w`],
+    src: upwork5_511,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcset: [`${upwork6_200} 200w`, `${upwork6_318} 318w`, `${upwork6_576} 576w`],
+    src: upwork6_576,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcset: [`${upwork7_200} 200w`, `${upwork7_576} 576w`],
+    src: upwork7_576,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcset: [`${upwork8_200} 200w`, `${upwork8_375} 375w`, `${upwork8_576} 576w`],
     src: upwork8_576,
     alt: 'upwork',
     width: 3,
     height: 4
   },
-
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcset: [`${upwork9_200} 200w`, `${upwork9_576} 576w`],
+    src: upwork9_576,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 576) 100vw', '576px'],
+    srcset: [`${upwork10_200} 200w`, `${upwork10_375} 375w`, `${upwork10_576} 576w`],
+    src: upwork10_576,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
   {
     sizes: ['(max-width: 576) 100vw', '576px'],
     srcSet: [`${upwork11_200} 200w`, `${upwork11_576} 576w`],
     src: upwork11_576,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 542) 100vw', '542px'],
+    srcSet: [`${upwork13_200} 200w`, `${upwork13_396} 396w`, `${upwork13_542} 542w`],
+    src: upwork13_542,
+    alt: 'upwork',
+    width: 4,
+    height: 3
+  },
+  {
+    sizes: ['(max-width: 534) 100vw', '534px'],
+    srcSet: [`${upwork14_200} 200w`, `${upwork14_388} 388w`, `${upwork14_534} 534w`],
+    src: upwork14_534,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 517) 100vw', '517px'],
+    srcSet: [`${upwork15_200} 200w`, `${upwork15_384} 384w`, `${upwork15_517} 517w`],
+    src: upwork15_517,
+    alt: 'upwork',
+    width: 3,
+    height: 4
+  },
+  {
+    sizes: ['(max-width: 591) 100vw', '591px'],
+    srcSet: [`${upwork16_200} 200w`, `${upwork16_372} 372w`, `${upwork16_492} 492w`, `${upwork16_591} 591w`],
+    src: upwork16_591,
     alt: 'upwork',
     width: 3,
     height: 4
