e36e18898574fdde63abb23d6b5615cc9c785dab
Adam <ablazek@securityscorecard.io>
2020-02-18 22:20:48 +0100

    add keywords using react-helmet

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
 import Header from './Header'
 import Footer from './Footer'
 import Welcome from './Welcome'
@@ -15,6 +16,16 @@ export const Desktop = (props) => <Responsive {...props} minWidth={767} />
 
 const App = () => (
   <div className="App">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Arbovert | Arboristika | Výškové práce | Praha</title>
+      <meta name="description" content="Arboristika a výškové práce" />
+      <meta
+        name="keywords"
+        content="hroty proti holubům,sítě proti holubům,výškové práce,práce ve výškách,mytí oken,mytí fasád čištění fasád,opravy fasád,péčě o dřeviny,likvidace dřeva,prořezávání stromů,arboristika,rizikové kácení stromů"
+      />
+      <link rel="canonical" href="https://arbovert.cz/" />
+    </Helmet>
     <Header />
 
     <Element name="welcome">
