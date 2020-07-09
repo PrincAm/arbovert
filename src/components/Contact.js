28f042e6fa7fddeb4a39104ce469a3859abea7ec
Adam <ablazek@securityscorecard.io>
2020-07-09 18:02:03 +0200

    add form.html

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ const Contact = () => {
     console.log(test)
     fetch('/', {
       method: 'POST',
-      // headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
       body: encode({
         'form-name': 'contact',
         ...data
